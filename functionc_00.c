5f1faf2a37d5531b67a736337010780e785f6510
HamzaDXTR101 <hamzanetro47@gmail.com>
2023-04-25 12:53:25 -0700

    functionc_00.c

---
+++ functionc_00.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_char - prints a char
+ * @types: list of arguments
+ * @buffer: buffer array to handle print
+ * @flags:  calculates active flags
+ * @width: width
+ * @precision: precision specification
+ * @size: specifies size
+ * Return: number of chars printed
+ */
+
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	char c = va_arg(types, int);
+
+	return (handle_write_char(c, buffer, flags, width, precision, size));
+}
+
+
