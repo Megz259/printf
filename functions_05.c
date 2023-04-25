7af851791bca39bce96d5e7b63a6b7787c0415d3
HamzaDXTR101 <hamzanetro47@gmail.com>
2023-04-25 12:57:25 -0700

    functions_05.c

---
+++ functions_05.c
@@ -0,0 +1,164 @@
+#include "main.h"
+
+/* Printing unsigned numbers*/
+
+/**
+ * print_unsigned - prints unsigned number
+ * @types: list of arguments
+ * @buffer: buffer array to handle print
+ * @flags:  calculates active flags
+ * @width: width
+ * @precision: precision specification
+ * @size: specifies size
+ * Return: number of chars printed
+ */
+
+int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+
+	num = convert_size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
+
+/*Printing - unsigned numbers (in octal)*/
+
+/**
+ * print_octal - prints unsigned number in octal
+ * @types: list of arguments
+ * @buffer: buffer array to handle print
+ * @flags:  calculates active flags
+ * @width: width
+ * @precision: precision specification
+ * @size: specifies size
+ * Return: number of chars printed
+ */
+
+int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
+
+	UNUSED(width);
+
+	num = convert_size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 8) + '0';
+		num /= 8;
+	}
+
+	if (flags & F_HASH && init_num != 0)
+		buffer[i--] = '0';
+
+	i++;
+
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
+
+/*Printing - unsigned numbers (in hexadecimal)*/
+
+/**
+ * print_hexadecimal - prints unsigned number in hexadecimal
+ * @types: list of arguments
+ * @buffer: buffer array to handle print
+ * @flags:  calculates active flags
+ * @width: width
+ * @precision: precision specification
+ * @size: specifies size
+ * Return: number of chars printed
+ */
+
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789abcdef", buffer,
+		flags, 'x', width, precision, size));
+}
+
+/*Printing - unsigned numbers (in upper hexadecimal)*/
+
+/**
+ * print_hexa_upper - prints unsigned number in upper hexadecimal
+ * @types: list of arguments
+ * @buffer: buffer array to handle print
+ * @flags:  calculates active flags
+ * @width: width
+ * @precision: precision specification
+ * @size: specifies size
+ * Return: number of chars printed
+ */
+
+int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789ABCDEF", buffer,
+		flags, 'X', width, precision, size));
+}
+
+/*Printing - hexadecimal numbers (in upper or lower)*/
+
+/**
+ * print_hexa - prints hexadecimal number in lower or upper
+ * @types: list of arguments
+ * @map_to: array of values to assign the number to
+ * @buffer: buffer array to handle print
+ * @flags:  calculates active flags
+ * @flag_ch: calculates active flags
+ * @width: width
+ * @precision: precision specification
+ * @size: specifies size
+ * Return: number of chars printed
+ */
+
+int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
+
+	UNUSED(width);
+
+	num = convert_size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[i--] = map_to[num % 16];
+		num /= 16;
+	}
+
+	if (flags & F_HASH && init_num != 0)
+	{
+		buffer[i--] = flag_ch;
+		buffer[i--] = '0';
+	}
+
+	i++;
+
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
